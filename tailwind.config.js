9815e9b14691f09f64cde26d4fee41d7ebfe1197
YoonJinJung <yj2135589@g.skku.edu>
2025-01-20 18:21:51 +0900

    fix: style fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,12 @@ export default {
           600: '#2563eb', // 체험수업 신청 버튼 기본 색상
           700: '#1d4ed8', // 호버 시 색상
         },
+        primary: '#343A40',
+        secondary: '#007BFF',
+        black: '#090909',
       },
       spacing: {
+        2: '0.5rem',
         4: '1rem', // p-4에 사용
         6: '1.5rem', // px-6에 사용
       },
