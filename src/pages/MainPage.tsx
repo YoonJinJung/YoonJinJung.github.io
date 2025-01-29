2a58ca77eab28488a52f3e00326d3b8cfb96dfce
YoonJinJung <yj2135589@g.skku.edu>
2025-01-29 11:19:45 +0900

    fix: 버튼 텍스트, 타이틀 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,13 +6,13 @@ export default function MainPage() {
     <div className='min-h-screen max-w-md bg-background flex flex-col items-center justify-center'>
       <Link to='/jy'>
         <Button className='bg-rose-400 text-[20px] px-10 py-10 hover:bg-neutral-500 mb-10'>
-          지율이 사랑해
+          4살 하지율
         </Button>
       </Link>
 
       <Link to='/ty'>
         <Button className='bg-teal-300 text-[20px] px-10 py-10 hover:bg-neutral-500'>
-          태율이 사랑해
+          2살 하태율
         </Button>
       </Link>
     </div>
